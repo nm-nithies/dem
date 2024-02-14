ded8e229c548884ce02dfa392317703821eadc19
nithies-nm <nithies.maruthachalam@multicorewareinc.com>
2024-02-14 09:37:55 +0530

    next for loop added

--- utilities/src.cpp
+++ utilities/src.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 int main()
 {
-    std::cout<<"hello";
+  
+    for(int i = 0; i < 5; i++)
+    {
+        std::cout<<i;
+    }
     for(int i = 0; i < 5; i++)
     {
         std::cout<<i;
